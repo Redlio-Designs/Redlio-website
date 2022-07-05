1701ec5779fb39d8e2daf6489fc10c557ee7f897
forammehtaa <foram14897@gmail.com>
2022-07-05 16:35:29 +0530

    new code

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../Header/Header.scss"
+
+export default function Header() {
+  return (
+    <div>
+        <div className='container'>
+            <h1>Heloo</h1>
+            <h2>Hi</h2>
+          
+        </div>
+
+    </div>
+  )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../components/Header'
+
+export default function Home() {
+  return (
+    <>
+    <Header/>
+    </>
+  )
+}
